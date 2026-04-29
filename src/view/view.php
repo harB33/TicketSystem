f9e54b000dc6bff58348118f1cbbdc271f1df08d
harB33 <bautistaharvy@gmail.com>
2026-04-29 21:12:04 +0800

    navbar arrow

--- src/view/view.php
+++ src/view/view.php
@@ -1,6 +1,8 @@
-<nav class="hidden">
-    <?php include './view/mobile/navBar/navBar.html'; ?>
-</nav>
-<!-- <nav class="lg:hidden sm:block">
-    <?php include './view/desktop/navBar/navBar.html'; ?>
-</nav> -->
+<mobile-view>
+    <nav class="block lg:hidden">
+        <?php include './view/mobile/navBar/navBar.html'; ?>
+    </nav>
+</mobile-view>
+<desktop-view>
+
+</desktop-view>

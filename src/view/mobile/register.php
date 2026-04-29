28c0147c8e82355cb69a0c8c433f88890e69f326
harB33 <bautistaharvy@gmail.com>
2026-04-30 01:10:57 +0800

    register

--- src/view/mobile/register.php
+++ src/view/mobile/register.php
@@ -1,3 +1,22 @@
-<nav>
-    <img src="./logo/register.png" alt="Logo">
-</nav>
+<logo class="h-[50%] w-full flex flex-col items-center justify-center">
+    <img src="./logo/register.png" alt="" class="w-2/3">
+</logo>
+<form action="post" class="flex flex-col h-[50%] w-full justify-center items-center -translate-y-[112px]">
+    <div class="flex flex-col items-center justify-start h-full w-[80%] gap-4">
+        <input type="text" placeholder="email" class="px-6 py-4 border border-primary rounded-full w-full text-xl font-bold">
+        <input type="password" placeholder="password" class="px-6 py-4 border border-primary rounded-full w-full text-xl font-bold">
+        <div class="grid grid-cols-3 w-full gap-4">
+            <div class="h-full w-full flex items-center justify-center border border-primary rounded-full p-2">
+            </div>
+            <div class="h-full w-full flex items-center justify-center border border-primary rounded-full p-2">
+            </div>
+            <div class="h-full w-full flex items-center justify-center border border-primary rounded-full p-2">
+            </div>
+        </div>
+        <input type="password" placeholder="confirm password" class="px-6 py-4 border border-primary rounded-full w-full text-xl font-bold">
+        <div class="flex items-center justify-end w-full">
+            <button type="submit" class="p-4 max-h-15.5 border border-primary rounded-full w-1/2 font-ballmer text-2xl">sign up</button>
+        </div>
+        </div>
+    </div>
+</form>

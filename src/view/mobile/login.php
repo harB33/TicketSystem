4c2f0289cf0ee5b63afee508ec75b43c77c24b24
harB33 <bautistaharvy@gmail.com>
2026-04-30 01:11:18 +0800

    index / login

--- src/view/mobile/login.php
+++ src/view/mobile/login.php
@@ -3,12 +3,15 @@
 </logo>
 <form action="post" class="flex flex-col h-[50%] w-full justify-center items-center">
     <div class="flex flex-col items-center justify-start h-full w-[80%] gap-4">
-        <input type="text" placeholder="email" class="px-6 py-4 border border-[#ff6699] rounded-full w-full text-xl font-bold">
-        <input type="password" placeholder="password" class="px-6 py-4 border border-[#ff6699] rounded-full w-full text-xl font-bold">
+        <input type="text" placeholder="email" class="px-6 py-4 border border-primary rounded-full w-full text-xl font-bold">
+        <input type="password" placeholder="password" class="px-6 py-4 border border-primary rounded-full w-full text-xl font-bold">
         <div class="flex items-center justify-between w-full">
-            <input type="checkbox" name="" id="" class="accent-[#ff6699]">
-            <button type="submit" class="p-4 max-h-15.5 border border-[#ff6699] rounded-full w-1/2 font-ballmer text-2xl">sign in</button>
+            <div class="flex items-center">
+                <input type="checkbox" checked="checked" class="checkbox border checkbox-primary" />
+                <span class="ml-2 text-sm ">Remember me</span>
+            </div>
+            <button type="submit" class="p-4 max-h-15.5 border border-primary rounded-full w-1/2 font-ballmer text-2xl">sign in</button>
         </div>
-        <p>Don't have an account? <a href="./register.php" class="text-[#ff6699]">Sign up</a></p>
+        <p>Don't have an account? <a href="./register.php" class="text-primary">Sign up</a></p>
     </div>
 </form>

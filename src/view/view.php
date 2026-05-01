71744b55ad9e449e8edaf6b7cdd51efc511d87c3
harB33 <bautistaharvy@gmail.com>
2026-05-01 19:58:16 +0800

    minor fix

--- src/view/view.php
+++ src/view/view.php
@@ -1,10 +1,23 @@
 <mobile-view class="relative z-10 block lg:hidden w-full h-screen bg-transparent">
-    <nav class="hidden">
-        <?php include __DIR__ . '/mobile/navBar.html'; ?>
-    </nav>
-    <login class="w-full h-screen flex flex-col">
-        <?php include __DIR__ . '/mobile/login.php'; ?>
+
+
+    <!-- Login Page -->
+    <login>
+        <div class="w-full h-screen flex flex-col">
+            <?php include __DIR__ . '/mobile/login.php'; ?>
+        </div>
     </login>
+
+
+    <!-- Register Page -->
+    <register>
+        <div class="w-full h-screen flex flex-col">
+            <?php include __DIR__ . '/mobile/register.php'; ?>
+        </div>
+    </register>
+
+
+    
 </mobile-view>
 <desktop-view>
 

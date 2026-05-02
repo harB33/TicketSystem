1dafc6b8d333be6bdbbf05f03d4cfd9a4248bd27
Carl Adrian A. Tan <adielaide.adi@gmail.com>
2026-05-03 04:30:53 +0800

    Added Classes in connectDB

--- src/ticket_db/connectdb.php
+++ src/ticket_db/connectdb.php
@@ -1,15 +1,42 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "staged_db";
-$port = 3306;
-$alternative_port = 3307;
-
-try {
-    $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
-} catch (Exception $e) {
-    $conn = mysqli_connect($servername, $username, $password, $dbname, $alternative_port);
+class DatabaseConfig {
+    public const SERVERNAME = "127.0.0.1";
+    public const USERNAME = "root";
+    public const PASSWORD = "";
+    public const DBNAME = "staged_db";
+    public const PORT_PRIMARY = 3306;
+    public const PORT_FALLBACK = 3307;
 }
+
+
+function connectToDatabase(): ?mysqli {
+    $servername = DatabaseConfig::SERVERNAME;
+    $username = DatabaseConfig::USERNAME;
+    $password = DatabaseConfig::PASSWORD;
+    $dbname = DatabaseConfig::DBNAME;
+
+    $conn = @mysqli_connect($servername, $username, $password, $dbname, DatabaseConfig::PORT_PRIMARY);
+    if ($conn && mysqli_ping($conn)) {
+        return $conn;
+    }
+
+    error_log("Primary connection failed. Trying fallback port.");
+    $conn = @mysqli_connect($servername, $username, $password, $dbname, DatabaseConfig::PORT_FALLBACK);
+
+    if ($conn && mysqli_ping($conn)) {
+        return $conn;
+    }
+
+    error_log("FATAL: Could not connect to the database.");
+    return null;
+}
+
+$conn = connectToDatabase();
+
+if ($conn === null) {
+    die("<h1 style='color: red;'>Error</h1><p>Connection failed. Please check server status and configuration.</p>");
+}
+
+mysqli_close($conn); 
 
 ?>

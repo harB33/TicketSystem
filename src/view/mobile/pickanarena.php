9706a9ae5d2b065b02040e37adbfdf5b675642a8
harB33 <bautistaharvy@gmail.com>
2026-05-02 22:41:48 +0800

    minor fix

--- src/view/mobile/pickanarena.php
+++ src/view/mobile/pickanarena.php
@@ -48,6 +48,6 @@
         </label>
     </div>
     <div class="p-10 fixed bottom-0 bg-linear-to-t from-black to-black/0 h-[25%] w-full flex flex-col justify-end items-center z-20">
-        <button onclick="window.location.href='?page=login';" class="bg-primary max-w-2xl max-h-13 text-white p-2 rounded-full w-1/2"><p class="font-ballmer text-2xl translate-y-1">next</p></button>
+        <button onclick="window.location.href='?page=featured';" class="bg-primary max-w-2xl max-h-13 text-white p-2 rounded-full w-1/2"><p class="font-ballmer text-2xl translate-y-1">next</p></button>
     </div>
 </div>

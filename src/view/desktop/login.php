f40a8093a7d2ac44d7fdc52db202f4c5f692cf7f
harB33 <bautistaharvy@gmail.com>
2026-05-11 01:11:49 +0800

    login desktop

--- src/view/desktop/login.php
+++ src/view/desktop/login.php
@@ -34,23 +34,25 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <source src="./asset/image/login.mp4" type="video/mp4">
 </video>
 <div class="absolute inset-0 bg-black/75 z-5"></div>
-<logo class="relative z-10 h-[50%] w-full flex flex-col items-center justify-center">
-    <img src="./asset/logo/logo.png" alt="" class="w-1/2">
-</logo>
-<form action="" method="post" id="loginForm" class="relative z-10 flex flex-col h-[50%] w-full justify-center items-center">
-    <div class="flex flex-col items-center justify-start h-full w-[80%] gap-4">
-        <input type="text" id="email" name="email" placeholder="email" class="px-6 py-4 rounded-full w-full text-lg text-[#525252] font-bold bg-[#919191]">
-        <input type="password" id="password" name="password" placeholder="password" class="px-6 py-4 rounded-full w-full text-lg text-[#525252] font-bold bg-[#919191]">
-        <div class="flex items-center justify-between w-full">
-            <div class="flex items-center">
-                <input type="checkbox" checked="checked" class="checkbox border checkbox-primary rounded-full" style="border-radius: 100% !important; box-shadow: none !important;" />
-                <span class="ml-2 text-sm opacity-75">Remember me</span>
+<div class="w-full h-full flex flex-col items-end">
+    <logo class="relative z-10 h-[50%]  w-[40%] flex flex-col items-center justify-center bg-black/50 backdrop-blur-2xl">
+        <img src="./asset/logo/logo.png" alt="" class="min-w-40 max-w-40">
+    </logo>
+    <form action="" method="post" id="loginForm" class="relative z-10 flex flex-col w-[40%] h-[50%] bg-black/50 backdrop-blur-2xl justify-center items-center">
+        <div class="flex flex-col items-center justify-start h-full w-[60%] gap-4">
+            <input type="text" id="email" name="email" placeholder="email" class="px-6 py-4 rounded-full w-full text-lg text-[#525252] font-bold bg-[#919191]">
+            <input type="password" id="password" name="password" placeholder="password" class="px-6 py-4 rounded-full w-full text-lg text-[#525252] font-bold bg-[#919191]">
+            <div class="flex items-center justify-between w-full">
+                <div class="flex items-center">
+                    <input type="checkbox" checked="checked" class="checkbox border checkbox-primary rounded-full" style="border-radius: 100% !important; box-shadow: none !important;" />
+                    <span class="ml-2 text-sm opacity-75">Remember me</span>
+                </div>
+                <button type="submit" class=" p-2 border border-primary rounded-full w-1/2 bg-primary"><p class="font-ballmer text-lg translate-y-1">sign in</p></button>
             </div>
-            <button type="submit" class=" p-2 border border-primary rounded-full w-1/2 bg-primary"><p class="font-ballmer text-lg translate-y-1">sign in</p></button>
+            <p class="opacity-75">Don't have an account? <a href="?page=register" class="text-primary">Sign up</a></p>
         </div>
-        <p class="opacity-75">Don't have an account? <a href="?page=register" class="text-primary">Sign up</a></p>
-    </div>
-</form>
+    </form>
+</div>
 
 <script>
 document.addEventListener('DOMContentLoaded', () => {
